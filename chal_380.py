73b08a520ed332a798cbaa5e9d82587966e4a6df
jameswmccarty <jameswmccarty@gmail.com>
2021-05-02 18:41:25 -0400

    Add files via upload

--- chal_380.py
+++ chal_380.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+from collections import deque
+import time
+
 """
 [2019-08-05] Challenge #380 [Easy] Smooshed Morse Code 1
 
@@ -112,3 +115,86 @@ for item in all_13:
 	if not used:
 		not_incl.add(item)
 print(not_incl)
+
+"""
+
+[2019-08-07] Challenge #380 [Intermediate] Smooshed Morse Code 2
+https://www.reddit.com/r/dailyprogrammer/comments/cn6gz5/20190807_challenge_380_intermediate_smooshed/
+
+Smooshed Morse code means Morse code with the spaces or other delimiters between encoded letters left out. See this week's Easy challenge for more detail.
+
+A permutation of the alphabet is a 26-character string in which each of the letters a through z appears once.
+
+Given a smooshed Morse code encoding of a permutation of the alphabet, find the permutation it encodes, or any other permutation that produces the same encoding (in general there will be more than one). It's not enough to write a program that will eventually finish after a very long period of time: run your code through to completion for at least one example.
+Examples
+
+smalpha(".--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..")
+    => "wirnbfzehatqlojpgcvusyxkmd"
+smalpha(".----...---.-....--.-........-----....--.-..-.-..--.--...--..-.---.--..-.-...--..-")
+    => "wzjlepdsvothqfxkbgrmyicuna"
+smalpha("..-...-..-....--.---.---.---..-..--....-.....-..-.--.-.-.--.-..--.--..--.----..-..")
+    => "uvfsqmjazxthbidyrkcwegponl"
+
+Again, there's more than one valid output for these inputs.
+Optional bonus 1
+
+Here's a list of 1000 inputs. How fast can you find the output for all of them? A good time depends on your language of choice and setup, so there's no specific time to aim for.
+Optional bonus 2
+
+Typically, a valid input will have thousands of possible outputs. The object of this bonus challenge is to find a valid input with as few possible outputs as possible, while still having at least 1. The following encoded string has 41 decodings:
+
+......-..--...---.-....---...--....--.-..---.....---.-.---..---.-....--.-.---.-.--
+
+Can you do better? When this post is 7 days old, I'll award +1 gold medal flair to the submission with the fewest possible decodings. I'll break ties by taking the lexicographically first string. That is, I'll look at the first character where the two strings differ and award the one with a dash (-) in that position, since - is before . lexicographically.
+
+"""
+
+alpha_set = { chr(i) for i in range(65,65+26) }
+
+# BFS
+def find_perm(morse):
+	q = deque()
+	q.append(('',alpha_set,morse))
+	while len(q) > 0:
+		alpha_str, a_set, m_str = q.popleft()
+		if len(alpha_str) == 26:
+			return alpha_str
+		for elem in a_set:
+			if morse_dict[elem] == m_str[0:len(morse_dict[elem])]:
+				new_alpha_set = a_set.copy()
+				new_alpha_set.discard(elem)
+				q.append((alpha_str+elem, new_alpha_set, m_str[len(morse_dict[elem]):]))
+	return None
+
+# DFS
+def find_perm2(morse, alpha='',a_set=alpha_set):
+	if len(morse) == 0:
+		return alpha
+	for char in a_set:
+		if morse_dict[char] == morse[0:len(morse_dict[char])]:
+			next_set = a_set.copy()
+			next_set.discard(char)
+			sol = find_perm2(morse[len(morse_dict[char]):], alpha+char, next_set)
+			if sol != False:
+				return sol
+	return False
+
+# trial
+sol = find_perm2('.--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..')
+print(sol)
+print(smorse(sol))
+print(smorse("wirnbfzehatqlojpgcvusyxkmd".upper()))
+
+print("Starting time trials")
+start_time = time.time()
+with open("smorse2-bonus1.in", "r") as infile:
+	for line in infile.readlines():
+		sub_time_start = time.time()
+		sol = find_perm2(line.strip())
+		if sol == None:
+			print("Failed to find solution for: ", line)
+			exit(0)
+		else:
+			sub_time_stop = time.time()
+			print(line.strip(), sol, str(sub_time_stop-sub_time_start))
+print("Total Time: ", str(time.time()-start_time))
