8d04a039b06e7e05836d6e01db3bfd97e5100aee
James McCarty <jameswmccarty@gmail.com>
2021-08-22 10:41:30 -0400

    add file

---
+++ chal_358.py
@@ -0,0 +1,85 @@
+"""
+
+https://www.reddit.com/r/dailyprogrammer/comments/8eger3/20180423_challenge_358_easy_decipher_the_seven/
+
+[2018-04-23] Challenge #358 [Easy] Decipher The Seven Segments
+Description
+
+Today's challenge will be to create a program to decipher a seven segment display, commonly seen on many older electronic devices.
+Input Description
+
+For this challenge, you will receive 3 lines of input, with each line being 27 characters long (representing 9 total numbers), with the digits spread across the 3 lines. Your job is to return the represented digits. You don't need to account for odd spacing or missing segments.
+Output Description
+
+Your program should print the numbers contained in the display.
+Challenge Inputs
+
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+
+    _  _  _  _  _  _  _  _ 
+|_| _| _||_|| ||_ |_| _||_ 
+  | _| _||_||_| _||_||_  _|
+
+ _  _  _  _  _  _  _  _  _ 
+|_  _||_ |_| _|  ||_ | ||_|
+ _||_ |_||_| _|  ||_||_||_|
+
+ _  _        _  _  _  _  _ 
+|_||_ |_|  || ||_ |_ |_| _|
+ _| _|  |  ||_| _| _| _||_ 
+
+Challenge Outputs
+
+123456789
+433805825
+526837608
+954105592
+
+"""
+
+def decode(txt):
+
+	digit_map = { 	' _ | ||_|' : '0',
+					'     |  |' : '1',
+					' _  _||_ ' : '2',
+					' _  _| _|' : '3',
+					'   |_|  |' : '4',
+					' _ |_  _|' : '5',
+					' _ |_ |_|' : '6',
+					' _   |  |' : '7',
+					' _ |_||_|' : '8',
+					' _ |_| _|' : '9'}
+
+	out = ''
+	span   = len(txt)//3
+	for i in range(span//3):
+		c = ''.join([txt[i*3+j*span:i*3+j*span+3] for j in range(3)])
+		out += '?' if c not in digit_map else digit_map[c]
+	return out
+
+l1 = "    _  _     _  _  _  _  _ "
+l2 = "  | _| _||_||_ |_   ||_||_|"
+l3 = "  ||_  _|  | _||_|  ||_| _|"
+
+print(decode(l1+l2+l3))
+
+l1 = "    _  _  _  _  _  _  _  _ "
+l2 = "|_| _| _||_|| ||_ |_| _||_ "
+l3 = "  | _| _||_||_| _||_||_  _|"
+
+print(decode(l1+l2+l3))
+
+l1 = " _  _  _  _  _  _  _  _  _ "
+l2 = "|_  _||_ |_| _|  ||_ | ||_|"
+l3 = " _||_ |_||_| _|  ||_||_||_|"
+
+print(decode(l1+l2+l3))
+
+l1 = " _  _        _  _  _  _  _ "
+l2 = "|_||_ |_|  || ||_ |_ |_| _|"
+l3 = " _| _|  |  ||_| _| _| _||_ "
+
+print(decode(l1+l2+l3))
+
