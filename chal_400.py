43e4555ddec27f01b8b954f60ec27534ae7362e6
James McCarty <jameswmccarty@gmail.com>
2023-06-03 17:22:35 -0400

    Add additional solution method

--- chal_400.py
+++ chal_400.py
@@ -22,7 +22,7 @@ You should be able to handle numbers up to 10,000 efficiently. The sum of all pr
 
 Optional bonus challenge
 
-Consider the numbers X in the range 1 to 10,000 inclusive. The sum of all X such that 1019 + X is a practical number is 1,451,958. Find the sum of all X such that 1020 + X is a practical number. I found the section Characterization of practical numbers in the Wikipedia article useful here. 
+Consider the numbers X in the range 1 to 10,000 inclusive. The sum of all X such that 10^19 + X is a practical number is 1,451,958. Find the sum of all X such that 10^20 + X is a practical number. I found the section Characterization of practical numbers in the Wikipedia article useful here. 
 
 https://en.wikipedia.org/wiki/Practical_number
 https://www.youtube.com/watch?v=IlZOLwf87gM
@@ -30,7 +30,7 @@ https://www.youtube.com/watch?v=IlZOLwf87gM
 
 """
 
-# return a list of a number's factors (largest first)
+# return a list of a number's divisors (largest first)
 def factor(num):
 	return [ i for i in range(1,num//2+1) if num%i == 0 ][::-1]
 
@@ -60,11 +60,10 @@ def prime_factor2(num):
 	i = 3
 	while i * i <= num:
 		if num%i == 0:
-			if i in factors:
+			factors[i] = 0
+			while num%i == 0:
 				factors[i] += 1
-			else:
-				factors[i] = 1
-			num //= i
+				num //= i
 		else:
 			i += 2
 	if num != 1:
@@ -86,6 +85,7 @@ def sums_to(val,num_list,cap):
 	return False
 
 # return True if num is a practical number
+# Perform check by finding a valid summation
 def practical(num):
 	if num != 1 and num%2 == 1:
 		return False
@@ -99,7 +99,7 @@ def sigma_check(p,p_last_sum):
 	total = p_last_sum + 1
 	if p <= total:
 		return True
-	for k in range(p_last_sum//2+1,0,-1):
+	for k in range(1,p_last_sum//2+1):
 		if p_last_sum%k == 0:
 			total += k
 			if p <= total:
@@ -107,6 +107,7 @@ def sigma_check(p,p_last_sum):
 	return False
 
 # return True if num is a practical number
+# Use prime factorization rules
 def practical2(num):
 	if num != 1 and num%2 == 1:
 		return False
@@ -114,6 +115,8 @@ def practical2(num):
 		return False
 	prime_count = prime_factor2(num)
 	prime_factors = sorted(prime_count.keys())
+	if len(prime_factors) == 1 and prime_factors[0] == 2:
+		return True
 	for i in range(1,len(prime_factors)):
 		p_sum = 1
 		for j in range(i):
@@ -121,6 +124,54 @@ def practical2(num):
 		if not sigma_check(prime_factors[i],p_sum):
 			return False
 	return True
+
+# return True if num is a practical number
+# Prime factorization can be slow...check as we go
+def practical3(num):
+	if num != 1 and num%2 == 1:
+		return False
+	if num > 2 and not (num%4 == 0 or num%6 == 0):
+		return False
+	factors = dict()
+	p_sum = 1
+	last_len = 0
+	if num % 2 == 0:
+		factors[2] = 0
+		while num % 2 == 0:
+			factors[2] += 1
+			num //= 2
+	if len(factors.keys()) > 0:
+		last_len = 1
+	i = 3
+	while i * i <= num:
+		if num%i == 0:
+			factors[i] = 0
+			while num%i == 0:
+				factors[i] += 1
+				num //= i
+		if len(factors.keys()) > last_len:
+			last_len += 1
+			if last_len > 1:
+				prime_factors = sorted(factors.keys())
+				p_sum *= (prime_factors[-2] ** factors[prime_factors[-2]])
+				if not sigma_check(prime_factors[-1],p_sum):
+					return False
+		i += 2
+	if num != 1:
+		if num in factors:
+			factors[num] += 1
+		else:
+			factors[num] = 1
+	if len(factors.keys()) > last_len:
+		prime_factors = sorted(factors.keys())
+		last_len += 1
+		if last_len > 1:
+			prime_factors = sorted(factors.keys())
+			p_sum *= (prime_factors[-2] ** factors[prime_factors[-2]])
+			if not sigma_check(prime_factors[-1],p_sum):
+				return False
+	return True
+
 
 print(practical(1))
 print(practical(2))
@@ -133,6 +184,12 @@ print(practical2(2))
 print(practical2(3))
 print(practical2(10))
 print(practical2(12))
+print()
+print(practical3(1))
+print(practical3(2))
+print(practical3(3))
+print(practical3(10))
+print(practical3(12))
 
 #print(prime_factor(100))
 #print(prime_factor(429606))
@@ -141,14 +198,9 @@ print(practical2(12))
 
 total = 0
 for i in range(1,10001):
-	if practical2(i):
+	if practical3(i):
 		total += i
 print(total)
 
-total = 0
 offset = 10000000000000000000
-for i in range(1,10001):
-	#print(i)
-	if practical2(i+offset):
-		total += i
-print(total)
+print(sum([ i for i in range(1,10001) if practical3(i+offset) ]))
